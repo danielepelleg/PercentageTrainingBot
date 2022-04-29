781c65f15d2cca42562b15951747ea337d633b86
danielepelleg <daniele.pellegrini@studenti.unipr.it>
2022-04-29 12:34:29 +0200

    Migrate to PostgreSQL Database

--- bot/database_manager.py
+++ bot/database_manager.py
@@ -1,109 +1,121 @@
-import sqlite3
+import psycopg2
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 def dict_from_row(row):
     return dict(zip(row.keys(), row))
 
 class DBManager(object):
     def __init__(self):
-        self.database_name = 'training-bot.db'
+        self.DATABASE_NAME = os.environ.get('DATABASE_NAME')
+        self.DATABASE_HOST = os.environ.get('DATABASE_HOST')
+        self.DATABASE_PORT = os.environ.get('DATABASE_PORT')
+        self.DATABASE_USER = os.environ.get('DATABASE_USER')
+        self.DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+        self.DATABASE_SSLMODE = os.environ.get('DATABASE_SSLMODE')
         self.__create_tables()
 
-    def create_connection(self, db_file):
+    def create_connection(self):
         """ create a database connection to the SQLite database
-            specified by the db_file
-        :param db_file: database file
+        :param db_url: database url
         :return: Connection object or None
         """
         conn = None
         try:
-            conn = sqlite3.connect(db_file)
-        except sqlite3.Error as e:
+            conn = psycopg2.connect(database = self.DATABASE_NAME,
+                                    user = self.DATABASE_USER,
+                                    password = self.DATABASE_PASSWORD,
+                                    host = self.DATABASE_HOST,
+                                    port = self.DATABASE_PORT,
+                                    sslmode = self.DATABASE_SSLMODE)
+        except psycopg2.Error as e:
             print(e)
 
         return conn
 
     def __create_tables(self):
-        connection = self.create_connection(self.database_name)
+        connection = self.create_connection()
         database = connection.cursor()
         # User (ID, Username)
-        database.execute("CREATE TABLE if not exists user (id INTEGER PRIMARY KEY, username TEXT)")
-        database.execute("CREATE TABLE if not exists training (user_id INTEGER PRIMARY KEY, training_name TEXT, FOREIGN KEY (user_id) REFERENCES user (id))")
-        database.execute("CREATE TABLE if not exists exercise (user_id INTEGER PRIMARY KEY, bench_press INTEGER, deadlift INTEGER, squat INTEGER, clean INTEGER, snatch INTEGER, jerk INTEGER, FOREIGN KEY (user_id) REFERENCES user (id))")
+        database.execute("CREATE TABLE IF NOT EXISTS \"user\" (id INTEGER PRIMARY KEY, username TEXT)")
+        database.execute("CREATE TABLE IF NOT EXISTS training (user_id INTEGER PRIMARY KEY, training_name TEXT, FOREIGN KEY (user_id) REFERENCES \"user\" (id))")
+        database.execute("CREATE TABLE IF NOT EXISTS exercise (user_id INTEGER PRIMARY KEY, bench_press INTEGER, deadlift INTEGER, back_squat INTEGER, clean INTEGER, snatch INTEGER, jerk INTEGER, front_squat INTEGER, thruster INTEGER, push_press INTEGER, shoulder_press INTEGER, overhead_squat INTEGER, FOREIGN KEY (user_id) REFERENCES \"user\" (id))")
         connection.commit()
         connection.close()
 
     def get_user(self, user_id):
-        connection = self.create_connection(self.database_name)
-        connection.row_factory = sqlite3.Row
-        database = connection.cursor()
-        query = database.execute(f"SELECT id, username FROM user WHERE id = {user_id}").fetchall()
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT id, username FROM \"user\" WHERE id = {user_id}")
         connection.commit()
+        result = cursor.fetchone()
         connection.close()
         try:
-            if query:
-                return dict_from_row(query[0])
+            if result:
+                return {'id': result[0], 'username': result[1]}
         except ValueError:
             return None
 
     def insert_user(self, user_id, username):
-        connection = self.create_connection(self.database_name)
-        database = connection.cursor()
-        database.execute(f"INSERT INTO user VALUES ({user_id}, '{username}')")
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO \"user\" VALUES ({user_id}, '{username}')")
         connection.commit()
         connection.close()
     
     def get_training(self, user_id):
-        connection = self.create_connection(self.database_name)
-        connection.row_factory = sqlite3.Row
-        database = connection.cursor()
-        query = database.execute(f"SELECT user_id, training_name FROM training WHERE user_id = {user_id}").fetchall()
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT user_id, training_name FROM training WHERE user_id = {user_id}")
         connection.commit()
+        result = cursor.fetchone()
         connection.close()
         try:
-            if query:
-                result = dict_from_row(query[0])
-                return result['training_name']
+            if result:
+                return result[1]
         except ValueError:
             return None
 
     def insert_training(self, user_id, training_name):
-        connection = self.create_connection(self.database_name)
-        database = connection.cursor()
-        database.execute(f"INSERT INTO training VALUES ({user_id}, '{training_name}')")
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO training VALUES ({user_id}, '{training_name}')")
         connection.commit()
         connection.close()
     
     def update_training(self, user_id, training_name):
-        connection = self.create_connection(self.database_name)
-        database = connection.cursor()
-        database.execute(f"UPDATE training SET training_name = '{training_name}' WHERE user_id = {user_id}")
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"UPDATE training SET training_name = '{training_name}' WHERE user_id = {user_id}")
         connection.commit()
         connection.close()
     
     def insert_exercise(self, user_id):
-        connection = self.create_connection(self.database_name)
-        database = connection.cursor()
-        database.execute(f"INSERT INTO exercise(user_id) VALUES ({user_id})")
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO exercise(user_id) VALUES ({user_id})")
         connection.commit()
         connection.close()
 
     def get_exercise(self, user_id, exercise_name):
-        connection = self.create_connection(self.database_name)
-        connection.row_factory = sqlite3.Row
-        database = connection.cursor()
-        query = database.execute(f"SELECT {exercise_name} FROM exercise WHERE user_id = {user_id}").fetchall()
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT {exercise_name} FROM exercise WHERE user_id = {user_id}")
         connection.commit()
+        result = cursor.fetchone()
         connection.close()
         try:
-            if query:
-                result = dict_from_row(query[0])
-                return result[f'{exercise_name}']
+            if result:
+                print(result[0])
+                return result[0]
         except ValueError:
             return None
     
     def update_exercise(self, user_id, column_name, exercise_rm):
-        connection = self.create_connection(self.database_name)
-        database = connection.cursor()
-        database.execute(f"UPDATE exercise SET {column_name} = {exercise_rm} WHERE user_id = {user_id}")
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"UPDATE exercise SET {column_name} = {exercise_rm} WHERE user_id = {user_id}")
         connection.commit()
         connection.close()
