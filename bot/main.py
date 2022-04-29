ddaca64df5b5f4cd5e12d827e50941bb97b28712
danielepelleg <daniele.pellegrini@studenti.unipr.it>
2022-04-29 16:27:15 +0200

    Reduce code complexity and Improve User Experience
    Delete useless if conditions. Add Keyboard's Buttons to make easier the user to get his tables.

--- bot/main.py
+++ bot/main.py
@@ -13,7 +13,8 @@ commands = {  # command description used in the "help" command
     'start'       : '\tGet used to the bot',
     'help'        : '\tShow available commands',
     'set'         : '\tSet the training type',
-    'exercise'    : '\tChoose the exercise to set'
+    'exercise'    : '\tChoose the exercise to set',
+    'tables'      : '\tShow the tables'
 }
 
 API_KEY = os.environ.get('API_KEY')
@@ -64,160 +65,96 @@ def set_training(message):
 @bot.message_handler(commands=['exercise'])
 def set_exercise(message):
     chat_id = message.chat.id
-    if not db_mng.get_training(chat_id):
+    user_training = db_mng.get_training(chat_id)
+    if not user_training:
         return bot.send_message(chat_id, """\
             Use the command /set to choose 
             the type of training first""")
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.row_width = 1
+    markup.row_width = 2
     markup.add(
-            telebot.types.InlineKeyboardButton("Bench Press", callback_data="bench_press"), \
-            telebot.types.InlineKeyboardButton("Deadlift", callback_data="deadlift"), \
-            telebot.types.InlineKeyboardButton("Squat", callback_data="squat"))
-    user_training = db_mng.get_training(chat_id)
+            telebot.types.InlineKeyboardButton("Bench Press", callback_data="bench_press"),
+            telebot.types.InlineKeyboardButton("Deadlift", callback_data="deadlift"),
+            telebot.types.InlineKeyboardButton("Back Squat", callback_data="back_squat"))
     if user_training == "crossfit":
         markup.add(
             telebot.types.InlineKeyboardButton("Clean", callback_data="clean"),
             telebot.types.InlineKeyboardButton("Snatch", callback_data="snatch"),
-            telebot.types.InlineKeyboardButton("Jerk", callback_data="jerk"))
+            telebot.types.InlineKeyboardButton("Jerk", callback_data="jerk"),
+            telebot.types.InlineKeyboardButton("Front Squat", callback_data="front_squat"),
+            telebot.types.InlineKeyboardButton("Thruster", callback_data="thruster"),
+            telebot.types.InlineKeyboardButton("Push Press", callback_data="snatch"),
+            telebot.types.InlineKeyboardButton("Shoulder Press", callback_data="push_press"),
+            telebot.types.InlineKeyboardButton("Overhead Squat", callback_data="overhead_squat"))
     bot.send_message(chat_id, '📈 Choose the exercise', reply_markup=markup)
 
-@bot.message_handler(commands=['bench_press', 'deadlift', 'squat', 'clean', 'snatch', 'jerk'])
-def handle_exercise(message):
+@bot.message_handler(commands=['tables'])
+def show_tables(message):
+    chat_id = message.chat.id
+    user_training = db_mng.get_training(chat_id)
+    if not user_training:
+        return bot.send_message(chat_id, """\
+            Use the command /exercise to set 
+            your exercise's PR first""")
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add('/bench_press', '/deadlift', '/back_squat')
+    if user_training == 'crossfit':
+        markup.add('/clean', '/snatch', '/jerk', '/front_squat',
+                    '/thruster', '/push_press', '/shoulder_press', '/overhead_squat')
+    bot.reply_to(message, 'Choose your PR\'s table', reply_markup=markup)
+
+@bot.message_handler(commands=["bench_press", "deadlift", "back_squat", "clean", 
+                                "snatch", "jerk", "front_squat", "thruster", 
+                                "push_press", "shoulder_press", "overhead_squat"])
+def handle_table(message):
     chat_id = message.chat.id
     exercise_name = str(message.text[1:])
     if not db_mng.get_training(chat_id):
         return bot.send_message(chat_id, """\
-            Use the command /exercise to 
-            set your exercise's PR first""")
+            Use the command /exercise to set
+            your exercise's PR first""")
     exercise_pr = db_mng.get_exercise(chat_id, exercise_name)
     exercise_name = exercise_name.replace('_', ' ')
     table_title = str(exercise_name.upper()).center(17, ' ')
     if exercise_pr != None:
-        bot.send_message(chat_id, f'**{table_title}**\n{draw_table(int(exercise_pr))}', parse_mode='Markdown')
+        bot.send_message(chat_id, f'*{table_title}*\n{draw_table(int(exercise_pr))}', parse_mode='Markdown')
     else:
         bot.reply_to(message, """\
-            Use the command /exercise to
-            set your exercise's PR first""")
+            Use the command /exercise to set
+            your exercise's PR first""")
 
 @bot.callback_query_handler(lambda call: call.data in ["powerlifting", "crossfit"])
 def callback_query_training(call):
     chat_id = call.message.chat.id
-    bot.answer_callback_query(call.id, f"You chose {call.data}!")
+    str_exercise_name = call.data[0].upper() + call.data[1:]
+    bot.answer_callback_query(call.id, f"You chose {str_exercise_name}!")
     if not db_mng.get_training(chat_id):
         db_mng.insert_training(chat_id, call.data)
         db_mng.insert_exercise(chat_id)
     else: 
         db_mng.update_training(chat_id, call.data)
 
-@bot.callback_query_handler(lambda call: call.data in ["bench_press", "deadlift", "squat", "clean", "snatch", "jerk"])
+@bot.callback_query_handler(lambda call: call.data in ["bench_press", "deadlift", "back_squat", "clean", 
+                                                       "snatch", "jerk", "front_squat", "thruster", 
+                                                       "push_press", "shoulder_press", "overhead_squat"])
 def callback_query_exercise(call):
     chat_id = call.message.chat.id
-    if call.data == "bench_press":
-        msg = bot.send_message(chat_id, 'Insert your 1RM Bench Press:')
-        bot.register_next_step_handler(msg, process_bench_step)
-    elif call.data == "deadlift":
-        msg = bot.send_message(chat_id, 'Insert your 1RM Deadlift:')
-        bot.register_next_step_handler(msg, process_deadlift_step)
-    elif call.data == "squat":
-        msg = bot.send_message(chat_id, 'Insert your 1RM Squat:')
-        bot.register_next_step_handler(msg, process_squat_step)
-    elif call.data == "clean":
-        msg = bot.send_message(chat_id, 'Insert your 1RM Clean:')
-        bot.register_next_step_handler(msg, process_clean_step)
-    elif call.data == "snatch":
-        msg = bot.send_message(chat_id, 'Insert your 1RM Snatch:')
-        bot.register_next_step_handler(msg, process_snatch_step)
-    elif call.data == "jerk":
-        msg = bot.send_message(chat_id, 'Insert your 1RM Jerk:')
-        bot.register_next_step_handler(msg, process_jerk_step)
+    exercise = str(call.data).replace('_', ' ').split()
+    str_exercise = ' '.join([word[0].upper()+word[1:] for word in exercise])
+    bot.send_message(chat_id, f'Insert your 1RM {str_exercise}:')
+    bot.register_next_step_handler_by_chat_id(chat_id, process_exercise, str_exercise_name = str_exercise, exercise_name = call.data)
 
-def process_bench_step(message):
-    """ Bench Press Demand
+def process_exercise(message, str_exercise_name, exercise_name):
+    """ Exercise Demand
     """
     try:
         chat_id = message.chat.id
         exercise_rm = message.text
         if not exercise_rm.isdigit():
-            msg = bot.reply_to(message, '1RM should be a number. Please reinsert')
-            bot.register_next_step_handler(msg, process_bench_step)
+            bot.reply_to(message, f'1RM {str_exercise_name} should be a number. Please reinsert')
+            bot.register_next_step_handler_by_chat_id(chat_id, process_exercise, str_exercise_name = str_exercise_name, exercise_name = exercise_name)
             return
-        db_mng.update_exercise(chat_id, 'bench_press', exercise_rm)
-        bot.send_message(chat_id, 'Saved!')
-    except Exception as e:
-        bot.reply_to(message, 'An error occurred')
-
-def process_deadlift_step(message):
-    """ Deadlift Demand
-    """
-    try:
-        chat_id = message.chat.id
-        exercise_rm = message.text
-        if not exercise_rm.isdigit():
-            msg = bot.reply_to(message, '1RM should be a number. Please reinsert')
-            bot.register_next_step_handler(msg, process_deadlift_step)
-            return
-        db_mng.update_exercise(chat_id, 'deadlift', exercise_rm)
-        bot.send_message(chat_id, 'Saved!')
-    except Exception as e:
-        bot.reply_to(message, 'An error occurred')
-
-def process_squat_step(message):
-    """ Squat Demand
-    """
-    try:
-        chat_id = message.chat.id
-        exercise_rm = message.text
-        if not exercise_rm.isdigit():
-            msg = bot.reply_to(message, '1RM should be a number. Please reinsert')
-            bot.register_next_step_handler(msg, process_squat_step)
-            return
-        db_mng.update_exercise(chat_id, 'squat', exercise_rm)
-        bot.send_message(chat_id, 'Saved!')
-    except Exception as e:
-        bot.reply_to(message, 'An error occurred')
-
-def process_clean_step(message):
-    """ Clean Demand
-    """
-    try:
-        chat_id = message.chat.id
-        exercise_rm = message.text
-        if not exercise_rm.isdigit():
-            msg = bot.reply_to(message, '1RM should be a number. Please reinsert')
-            bot.register_next_step_handler(msg, process_clean_step)
-            return
-        db_mng.update_exercise(chat_id, 'clean', exercise_rm)
-        bot.send_message(chat_id, 'Saved!')
-    except Exception as e:
-        bot.reply_to(message, 'An error occurred')
-
-def process_snatch_step(message):
-    """ Snatch Demand
-    """
-    try:
-        chat_id = message.chat.id
-        exercise_rm = message.text
-        if not exercise_rm.isdigit():
-            msg = bot.reply_to(message, '1RM should be a number. Please reinsert')
-            bot.register_next_step_handler(msg, process_snatch_step)
-            return
-        db_mng.update_exercise(chat_id, 'snatch', exercise_rm)
-        bot.send_message(chat_id, 'Saved!')
-    except Exception as e:
-        bot.reply_to(message, 'An error occurred')
-
-def process_jerk_step(message):
-    """ Jerk Demand
-    """
-    try:
-        chat_id = message.chat.id
-        exercise_rm = message.text
-        if not exercise_rm.isdigit():
-            msg = bot.reply_to(message, '1RM should be a number. Please reinsert')
-            bot.register_next_step_handler(msg, process_jerk_step)
-            return
-        db_mng.update_exercise(chat_id, 'jerk', exercise_rm)
+        db_mng.update_exercise(chat_id, exercise_name, exercise_rm)
         bot.send_message(chat_id, 'Saved!')
     except Exception as e:
         bot.reply_to(message, 'An error occurred')
